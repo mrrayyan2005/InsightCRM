b3728a4c3cca3002eddace5e55db0a80f929debf
Mohd Rayyan <143685393+mrrayyan2005@users.noreply.github.com>
2025-10-04 00:37:06 +0530

    Update app.js

--- server/src/app.js
+++ server/src/app.js
@@ -8,7 +8,7 @@ const app = express();
 
 // More explicit CORS configuration for debugging
 app.use(cors({ 
-  origin: ["http://localhost:5173", "http://localhost:5174"], 
+  origin: ["http://localhost:5173", "http://localhost:5174","https://insight-crm-six.vercel.app/"],  
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
